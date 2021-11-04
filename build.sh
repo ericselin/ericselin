096b765126007d26be3d818d43247dd4cca7fb49
Eric Selin <eric.selin@gmail.com>
2021-11-04 13:04:35 +0100

    ci: Add deno bin to path in build script

--- build.sh
+++ build.sh
@@ -7,6 +7,11 @@ if [ -z "$BOB_VERSION" ]; then
   exit 1
 fi
 
+# Add deno bin to PATH, just in case.
+# This will be used both by the Deno installer (as per the default),
+# and as the install location for bob
+PATH="$HOME/.deno/bin:$PATH"
+
 # Install Deno if not already installed
 deno --version || curl -fsSL https://deno.land/x/install/install.sh | sh
 
